36535cafe58c7e678eddbca2986ed2e680edcb4c
Vahe Abazyan <va.abazyan@gmail.com>
2021-12-07 21:39:11 +0400

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Vahe Abazyan on 07.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
